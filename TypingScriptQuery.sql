853182124d2d278ee1c9803bb7b08d5e3bfa33dc
Alex <alex.mefistofelico@gmail.com>
2019-05-27 06:54:21 -0400

    Insercion Lecciones...

--- TypingScriptQuery.sql
+++ TypingScriptQuery.sql
@@ -1,43 +1,144 @@
-CREATE DATABASE TYPING
-
-GO 
-
 USE TYPING
+select * from LECCION
 
-CREATE TABLE USUARIO(
-	UsuarioID INT NOT NULL,
-	Modo INT,
-	Nombre VARCHAR(50),
-	PRIMARY KEY (UsuarioID)
-);
+--nivel 1
+INSERT INTO LECCION VALUES(1,'asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj
+asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj
+asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj
+asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj
+asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj asdf ñlkj'
+,30);
+INSERT INTO LECCION VALUES(2,'asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj
+asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj
+asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj
+asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj
+asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj asdfgf ñlkjhj
+',30);
+INSERT INTO LECCION VALUES(3,'dada dada dada dada dada dada dada dada dada dada dada dada dada
+dada dada dada dada dada dada dada dada dada dada dada dada dada
+dada dada dada dada dada dada dada dada dada dada dada dada dada
+',30);
+INSERT INTO LECCION VALUES(4,'fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa
+fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa
+fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa fasa
+',30);
+--nivel 2
+INSERT INTO LECCION VALUES(5,'daña daña daña daña daña daña daña daña daña daña daña daña daña
+daña daña daña daña daña daña daña daña daña daña daña daña daña
+daña daña daña daña daña daña daña daña daña daña daña daña daña
+',40);
+INSERT INTO LECCION VALUES(6,'asadas asadas asadas asadas asadas asadas asadas asadas asadas
+asadas asadas asadas asadas asadas asadas asadas asadas asadas
+asadas asadas asadas asadas asadas asadas asadas asadas asadas
+asadas asadas asadas asadas asadas asadas asadas asadas asadas
+asadas asadas asadas asadas asadas asadas asadas asadas asadas
+',40);
+INSERT INTO LECCION VALUES(7,'kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña
+kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña
+kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña
+kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña
+kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña kalaña
+',40);
+INSERT INTO LECCION VALUES(8,'awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu
+awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu
+awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu
+awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu
+awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu awer ñoiu
+',40);
+--nivel 3
+INSERT INTO LECCION VALUES(9,'tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo
+tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo
+tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo tuyo
+',50);
+INSERT INTO LECCION VALUES(10,'quesos quesos quesos quesos quesos quesos quesos quesos quesos
+quesos quesos quesos quesos quesos quesos quesos quesos quesos
+quesos quesos quesos quesos quesos quesos quesos quesos quesos
+quesos quesos quesos quesos quesos quesos quesos quesos quesos
+quesos quesos quesos quesos quesos quesos quesos quesos quesos
+',50);
+INSERT INTO LECCION VALUES(11,'tretas tretas tretas tretas tretas tretas tretas tretas tretas
+tretas tretas tretas tretas tretas tretas tretas tretas tretas
+tretas tretas tretas tretas tretas tretas tretas tretas tretas
+tretas tretas tretas tretas tretas tretas tretas tretas tretas
+tretas tretas tretas tretas tretas tretas tretas tretas tretas
+',50);
+INSERT INTO LECCION VALUES(12,'axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m
+axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m
+axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m
+axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m
+axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m axcv ñ.,m
+',50);
+--nivel 4
+INSERT INTO LECCION VALUES(13,'axcvbv ñ.,mnm axcvbv ñ.,mnm axcvbv ñ.,mnm axcvbv ñ.,mnm
+axcvbv ñ.,mnm axcvbv ñ.,mnm axcvbv ñ.,mnm axcvbv ñ.,mnm
+axcvbv ñ.,mnm axcvbv ñ.,mnm axcvbv ñ.,mnm axcvbv ñ.,mnm
+',60);
+INSERT INTO LECCION VALUES(14,'a23454 ñ98767 a23454 ñ98767 a23454 ñ98767 a23454 ñ98767
+a23454 ñ98767 a23454 ñ98767 a23454 ñ98767 a23454 ñ98767
+a23454 ñ98767 a23454 ñ98767 a23454 ñ98767 a23454 ñ98767
+a23454 ñ98767 a23454 ñ98767 a23454 ñ98767 a23454 ñ98767
+a23454 ñ98767 a23454 ñ98767 a23454 ñ98767 a23454 ñ98767
+',60);
+INSERT INTO LECCION VALUES(15,'45432a 76789ñ 45432a 76789ñ 45432a 76789ñ 45432a 76789ñ
+45432a 76789ñ 45432a 76789ñ 45432a 76789ñ 45432a 76789ñ
+45432a 76789ñ 45432a 76789ñ 45432a 76789ñ 45432a 76789ñ
+45432a 76789ñ 45432a 76789ñ 45432a 76789ñ 45432a 76789ñ
+45432a 76789ñ 45432a 76789ñ 45432a 76789ñ 45432a 76789ñ
+',60);
+INSERT INTO LECCION VALUES(16,'asdfgf awertr asdfgf awertr asdfgf awertr asdfgf awertr
+asdfgf awertr asdfgf awertr asdfgf awertr asdfgf awertr
+asdfgf awertr asdfgf awertr asdfgf awertr asdfgf awertr
+asdfgf awertr asdfgf awertr asdfgf awertr asdfgf awertr
+asdfgf awertr asdfgf awertr asdfgf awertr asdfgf awertr
+',60);
+--nivel 5
+INSERT INTO LECCION VALUES(17,'ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767
+ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767
+ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767
+ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767
+ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767 ñ.,mnm ñ98767
+',70);
+INSERT INTO LECCION VALUES(18,'0pñ- 9ol. 8ik, 7ujm 6yhn 5tgb 4rfv 3edc 2wsx 1qaz
+0pñ- 9ol. 8ik, 7ujm 6yhn 5tgb 4rfv 3edc 2wsx 1qaz
+0pñ- 9ol. 8ik, 7ujm 6yhn 5tgb 4rfv 3edc 2wsx 1qaz
+0pñ- 9ol. 8ik, 7ujm 6yhn 5tgb 4rfv 3edc 2wsx 1qaz
+0pñ- 9ol. 8ik, 7ujm 6yhn 5tgb 4rfv 3edc 2wsx 1qaz
+',70);
+INSERT INTO LECCION VALUES(19,'celo celo celo celo celo celo celo celo celo celo celo celo celo
+celo celo celo celo celo celo celo celo celo celo celo celo celo
+celo celo celo celo celo celo celo celo celo celo celo celo celo
+',70);
+INSERT INTO LECCION VALUES(20,'kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo
+kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo
+kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo kilo
+',70);
+--nivel 6
+INSERT INTO LECCION VALUES(21,'peso peso peso peso peso peso peso peso peso peso peso peso peso
+peso peso peso peso peso peso peso peso peso peso peso peso peso
+peso peso peso peso peso peso peso peso peso peso peso peso peso
+',80);
+INSERT INTO LECCION VALUES(22,'mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa
+mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa
+mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa mesa
+',80);
+INSERT INTO LECCION VALUES(23,'ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo
+ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo
+ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo ramo
+',80);
+INSERT INTO LECCION VALUES(24,'caña caña caña caña caña caña caña caña caña caña caña caña caña
+caña caña caña caña caña caña caña caña caña caña caña caña caña
+caña caña caña caña caña caña caña caña caña caña caña caña caña
+',80);
+--nivel 7
+INSERT INTO LECCION VALUES(25,'turco turco turco turco turco turco turco turco turco turco turco
+turco turco turco turco turco turco turco turco turco turco turco
+turco turco turco turco turco turco turco turco turco turco turco
+',90);
+INSERT INTO LECCION VALUES(26,'tribu tribu tribu tribu tribu tribu tribu tribu tribu tribu tribu
+tribu tribu tribu tribu tribu tribu tribu tribu tribu tribu tribu
+tribu tribu tribu tribu tribu tribu tribu tribu tribu tribu tribu
+',90);
+--INSERT INTO LECCION VALUES(27,'',90);
+--INSERT INTO LECCION VALUES(28,'',90);
 
-CREATE TABLE LECCION(
-	LeccionID INT NOT NULL,
-	Parrafo VARCHAR(600),
-	PPM INT,
-	PRIMARY KEY (LeccionID)
-);
 
-CREATE TABLE NIVEL(
-	NivelID INT NOT NULL,
-	UsuarioID INT,
-	LeccionID INT,
-	PRIMARY KEY(NivelID),
-	FOREIGN KEY(UsuarioID) REFERENCES USUARIO,
-	FOREIGN KEY(LeccionID) REFERENCES LECCION
-);
-
-CREATE TABLE PROGRESO(
-	ProgresoID INT NOT NULL,
-	UsuarioID INT,
-	Fecha DATETIME,
-	PPMC INT,
-	Leccion INT,
-	Nivel INT,
-	FOREIGN KEY(UsuarioID) REFERENCES USUARIO
-);
-
-SELECT * FROM USUARIO
-SELECT * FROM LECCION
-SELECT * FROM NIVEL
-SELECT * FROM PROGRESO
